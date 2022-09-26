1162154a1f46fd4a2d4d440f719ea9ef24ec1522
junedkaxioned <junedk@axioned.com>
2022-09-26 18:06:27 +0530

    JS Complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-/* Author: Zishan Ansari */
+
+var card = document.querySelectorAll(".card");
+
+card[0].classList.add('active');
+
+card.forEach(function(expand){
+  expand.addEventListener('click', function (clickEvent) {
+    for (var img of card) {
+      img.classList.remove('active');
+    }
+    clickEvent.target.classList.add('active');
+  })
+})
+
+
